2c1257ac9831a4df572be804a03514c2232564bf
Leslie Raganit <lnraganit@gmail.com>
2023-10-11 17:35:37 -0700

    fixed playwright.config.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,7 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+// to run use npx playwright test
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+import { test } from "@playwright/test";
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test("test", async ({ page }) => {
+  // page is authenticated
 });
