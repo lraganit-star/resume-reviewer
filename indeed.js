ee45ca6a788c038c689c445f7fffa3a30ec7e475
Leslie Raganit <lnraganit@gmail.com>
2023-10-17 18:05:53 -0700

    get data from cards

--- indeed.js
+++ indeed.js
@@ -8,12 +8,22 @@ async function main() {
   });
 
   const page = await browser.newPage({});
-
   await page.goto(
     "https://www.indeed.com/jobs?q=front+end+developer&l=san+francisco%2C+ca&sc=0kf%3Aattr%28DSQF7%29%3B&radius=100&pp=gQAAAAAAAAAAAAAAAAACE76pWQADAAABAAA&vjk=c71d3fc8e4d88f45"
   );
-
-  await await page.pause();
+  const jobInfo = await page.$eval(
+    "#mosaic-provider-jobcards",
+    (headerCard) => {
+      const data = [];
+      const listCards = headerCard.getElementsByClassName("css-5lfssm");
+      Array.from(listCards).forEach((card) => {
+        data.push(card.innerText.split("\n"));
+      });
+      return data;
+    }
+  );
+  //   await page.pause();
+  console.log(jobInfo);
 }
 
 main();
